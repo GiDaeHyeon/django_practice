5f8e6f90c00994c1ada61edada88fa5bc312a209
GiDaeHyeon <81670897+GiDaeHyeon@users.noreply.github.com>
2021-06-12 15:37:56 +0900

    [ADD] first templates

--- myproject/accountapp/views.py
+++ myproject/accountapp/views.py
@@ -1,8 +1,7 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 appname = "accountapp"
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse("Hello World!!")
+    return render(request, 'base.html')
